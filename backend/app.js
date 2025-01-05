8f36a103f9beac8a2d28269011802d1daf39a89b
Prajval Patel <prajval1420@gmail.com>
2025-01-05 17:37:03 -0500

    test

--- backend/app.js
+++ backend/app.js
@@ -1,45 +1,9 @@
-// Importing necessary libraries
 const express = require('express');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const path = require("path");
-const carRoutes = require('./routes/carRoutes');
-
-// Initialize the express application
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(cors(
-  {
-    origin: ["https://fast-lane-frontend.vercel.app/"],
-    methods: ["POST", "GET"],
-    credentials: true
-  }  
-));
-// Middleware to parse JSON bodies
-app.use(bodyParser.json());
-app.use("/uploads", express.static(path.join(__dirname, "uploads")));
+app.get('/', (req, res) => res.send('Hello World!'));
 
-// Connect to MongoDB
-mongoose.connect('mongodb+srv://prajval1420:znDPSV3LU3wjC64m@cluster0.opwfq.mongodb.net/carDealership?retryWrites=true&w=majority&appName=Cluster0', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-}).then(() => {
-  console.log('Connected to the MongoDB');
-}).catch(err => {
-  console.log('Failed to connect to MongoDB', err);
-});
-
-// Routes
-//app.use('/api/cars', carRoutes);
-app.get("/", (req, res) => {
-  res.status(200).send("Hello from Express!");
-});
-      
-// Starting the server
 app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+  console.log(`Server listening on port ${PORT}`);
 });
-
-module.exports = app;
