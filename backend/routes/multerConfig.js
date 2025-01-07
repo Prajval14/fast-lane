5461eb6224f3dbf1d0af580abcf25e59885e777f
Prajval Patel <prajval1420@gmail.com>
2025-01-06 22:19:38 -0500

    refactored

--- backend/routes/multerConfig.js
+++ backend/routes/multerConfig.js
@@ -4,9 +4,9 @@ const multerS3 = require('multer-s3');
 const path = require('path');
 
 aws.config.update({
-    secretAccessKey: 'AWS_SECRET_ACCESS_KEY',
-    accessKeyId: 'AWS_ACCESS_KEY_ID',
-    region: 'AWS_REGION' // Example: 'us-east-1'
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    region: process.env.AWS_REGION 
 });
 
 const s3 = new aws.S3();
@@ -14,7 +14,7 @@ const s3 = new aws.S3();
 const upload = multer({
     storage: multerS3({
         s3: s3,
-        bucket: 'AWS_BUCKET_NAME',
+        bucket: process.env.AWS_BUCKET_NAME,
         acl: 'public-read',
         contentType: multerS3.AUTO_CONTENT_TYPE,
         key: function (req, file, cb) {
