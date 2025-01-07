dbc26f53a7850837fab1573f2b71206f43adc74c
Prajval Patel <prajval1420@gmail.com>
2025-01-06 21:09:49 -0500

    refactored code

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,44 @@
+// Importing necessary libraries
 const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const path = require("path");
+const carRoutes = require('./routes/carRoutes');
+// Initialize the express application
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.get('/', (req, res) => res.send('Hello World!'));
-
-app.listen(PORT, () => {
-  console.log(`Server listening on port ${PORT}`);
+app.use(cors(
+  {
+    origin: ["https://fast-lane-frontend.vercel.app/"],
+    methods: ["POST", "GET"],
+    credentials: true
+  }  
+));
+// Middleware to parse JSON bodies
+app.use(bodyParser.json());
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
+// Connect to MongoDB
+mongoose.connect('mongodb+srv://prajval1420:znDPSV3LU3wjC64m@cluster0.opwfq.mongodb.net/carDealership?retryWrites=true&w=majority&appName=Cluster0', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+}).then(() => {
+  console.log('Connected to the MongoDB');
+}).catch(err => {
+  console.log('Failed to connect to MongoDB', err);
 });
+
+// Routes
+app.use('/api/cars', carRoutes);
+
+// Root endpoint
+app.get('/', (req, res) => {
+  res.send('Welcome to my Node.js app deployed on Vercel!');
+});
+      
+// Starting the server
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
+module.exports = app;
--- backend/routes/carRoutes.js
+++ backend/routes/carRoutes.js
@@ -3,10 +3,7 @@ const router = express.Router();
 const carController = require('../controllers/carController');
 const upload = require('./multerConfig');
 
-router.get('/', (req, res) => {
-    console.log("Fetching all cars");
-    carController.getAllCars(req, res);
-});
+router.get('/', carController.getAllCars);
 router.get('/:id', carController.getCar);
 router.post('/', upload, carController.createCar);
 router.put('/:id', upload, carController.updateCar);
