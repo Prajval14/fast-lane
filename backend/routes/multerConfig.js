91271c4c940dd7c23bef5be664bf67d525bac2f9
Prajval Patel <prajval1420@gmail.com>
2025-01-06 22:16:37 -0500

    configured aws to use cloud storage

--- backend/routes/multerConfig.js
+++ backend/routes/multerConfig.js
@@ -1,41 +1,43 @@
+const aws = require('aws-sdk');
 const multer = require('multer');
+const multerS3 = require('multer-s3');
 const path = require('path');
 
-const storage = multer.diskStorage({
-  destination: function(req, file, cb) {
-    console.log("Setting destination for file:", file.originalname);
-    cb(null, './uploads/');
-  },
-  filename: function(req, file, cb) {
-    const newFilename = file.fieldname + '-' + Date.now() + path.extname(file.originalname);
-    console.log("Generating filename:", newFilename);
-    cb(null, newFilename);
-  }
+aws.config.update({
+    secretAccessKey: 'AWS_SECRET_ACCESS_KEY',
+    accessKeyId: 'AWS_ACCESS_KEY_ID',
+    region: 'AWS_REGION' // Example: 'us-east-1'
 });
 
-function checkFileType(file, cb) {
-  const filetypes = /jpeg|jpg|png|gif/;
-  const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
-  const mimetype = filetypes.test(file.mimetype);
-
-  console.log("Checking file type:", file.originalname);
-  console.log("Extension name passed:", extname);
-  console.log("MIME type passed:", mimetype);
-
-  if (mimetype && extname) {
-    return cb(null, true);
-  } else {
-    console.log("Invalid file type:", file.originalname);
-    cb(new Error('Error: Only Images are Allowed!'));
-  }
-}
+const s3 = new aws.S3();
 
 const upload = multer({
-  storage: storage,
-  limits: { fileSize: 1000000 }, // 1MB limit
-  fileFilter: function(req, file, cb) {
-    checkFileType(file, cb);
-  }
+    storage: multerS3({
+        s3: s3,
+        bucket: 'AWS_BUCKET_NAME',
+        acl: 'public-read',
+        contentType: multerS3.AUTO_CONTENT_TYPE,
+        key: function (req, file, cb) {
+            const newFilename = file.fieldname + '-' + Date.now() + path.extname(file.originalname);
+            cb(null, newFilename);
+        }
+    }),
+    limits: { fileSize: 1000000 }, // 1MB limit
+    fileFilter: function(req, file, cb) {
+        checkFileType(file, cb);
+    }
 }).array('images', 4);
+
+function checkFileType(file, cb) {
+    const filetypes = /jpeg|jpg|png|gif/;
+    const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
+    const mimetype = filetypes.test(file.mimetype);
+
+    if (mimetype && extname) {
+        return cb(null, true);
+    } else {
+        cb(new Error('Error: Only Images are Allowed!'));
+    }
+}
 
 module.exports = upload;
